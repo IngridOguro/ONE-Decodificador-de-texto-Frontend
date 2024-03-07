7747b5da3230871a3a2eb7d3d6e1bd05b57be364
IS Oguro <ingrid.shizue@gmail.com>
2024-03-07 12:03:19 -0300

    verificação acentos e maiusculas

--- app.js
+++ app.js
@@ -3,12 +3,21 @@ function hidePlaceholderAndBorder() {
     input.classList.add('hide-placeholder', 'hide-border');
   }
   
-  function showPlaceholderAndBorder() {
-    let input = document.getElementById('section1__campo-input__input');
-    input.classList.remove('hide-placeholder', 'hide-border');
+function showPlaceholderAndBorder() {
+  let input = document.getElementById('section1__campo-input__input');
+  input.classList.remove('hide-placeholder', 'hide-border');
+  }
+
+function temLetrasMaiusculas(string) {
+  return /[A-Z]/.test(string);
+  }
+
+function temAcentos(string) {
+    return /[À-ÖØ-öø-ÿ]/.test(string);
   }
   
 function pegar_input(){
     let texto = document.getElementById('section1__campo-input__input').value;
-    alert(texto);
+    texto_s2 = document.getElementById("texto-section2");
+    texto_s2.innerHTML = `${temLetrasMaiusculas(texto) | temAcentos(texto)}`;
 }
