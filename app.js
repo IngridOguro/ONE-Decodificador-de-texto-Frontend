af5c8c706e2cf1efcf71ea925c3cfb1ccfa9e47b
IS Oguro <ingrid.shizue@gmail.com>
2024-04-03 21:10:34 -0300

    função criptografar e descriptografar

--- app.js
+++ app.js
@@ -8,16 +8,59 @@ function showPlaceholderAndBorder() {
   input.classList.remove('hide-placeholder', 'hide-border');
   }
 
-function temLetrasMaiusculas(string) {
-  return /[A-Z]/.test(string);
-  }
+// dicionario de criptografia
+const dict = {
+                a:"ai",
+                e:"enter",
+                i:"imes",
+                o:"ober",
+                u:"ufat"
+              };
 
-function temAcentos(string) {
-    return /[À-ÖØ-öø-ÿ]/.test(string);
+function getinput(){
+  let mensagem = document.getElementById('section1__campo-input__input').value;
+  return mensagem;   
+}              
+
+function getfield(){
+  let campo_mensagem_descriptografada = document.getElementById("texto-section2");
+  return campo_mensagem_descriptografada;
+}
+
+function saoMinusculas(str) {
+  let campo = getfield()
+  if(/^[a-z]+$/.test(str)==false){
+    campo = "! Apenas letras minusculas e sem acento.";
+  };
+}
+
+function Criptografar(){
+let input_cript = getinput();
+let field_cript = getfield();
+   let mensagem_descriptografada = "";
+   for (let letra = 0; letra < input_cript.length; letra++) {
+    for (let id = 0; id < Object.keys(dict).length;id++) {
+      let key = Object.keys(dict)[id].toString()
+      if(input_cript[letra].includes(key)){
+        let value = dict[Object.keys(dict)[id]].toString()
+        mensagem_descriptografada += value;
+      }
+    }
   }
-  
-function pegar_input(){
-    let texto = document.getElementById('section1__campo-input__input').value;
-    texto_s2 = document.getElementById("texto-section2");
-    texto_s2.innerHTML = `${temLetrasMaiusculas(texto) | temAcentos(texto)}`;
+  field_cript.innerHTML = mensagem_descriptografada;
+}               
+
+function Descriptografar(){
+  let input_desc = getinput();
+  let field_desc = getfield();
+  for (let id = 0; id < Object.keys(dict).length;id++) {
+    let value = dict[Object.keys(dict)[id]].toString()
+    let key = Object.keys(dict)[id].toString()
+    
+    if (input_desc.includes(value)) {
+      input_desc = input_desc.replace(new RegExp(value, 'g'),key);
+      console.log("value: "+value+" key: "+key+" input: "+input_desc);
+    }
+  }
+  field_desc.innerHTML = input_desc;
 }
